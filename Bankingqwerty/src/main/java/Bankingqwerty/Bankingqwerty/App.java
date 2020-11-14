21ebda82465923146396a2b5d8605390c5247cf4
Asheej M A <Asheej M A@AsheeJ>
2020-11-14 18:31:07 +0530

    initiate commit

---
+++ Bankingqwerty/src/main/java/Bankingqwerty/Bankingqwerty/App.java
@@ -0,0 +1,13 @@
+package Bankingqwerty.Bankingqwerty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
